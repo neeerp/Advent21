8cd97f93c63e2e9bf94c2698e35ba4f17a9aadcb
David Goldstein <goldstein.g.david@gmail.com>
2022-05-19 18:25:15 -0400

    Initial commit.

---
+++ lib/day1.ex
@@ -0,0 +1,18 @@
+defmodule DAY1 do
+  @moduledoc """
+  Documentation for `DAY1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DAY1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/day1_test.exs
@@ -0,0 +1,8 @@
+defmodule DAY1Test do
+  use ExUnit.Case
+  doctest DAY1
+
+  test "greets the world" do
+    assert DAY1.hello() == :world
+  end
+end
