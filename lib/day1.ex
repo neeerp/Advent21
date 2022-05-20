9c3fc545a22bc672693d15d65b4cf1c91f0259da
David Goldstein <goldstein.g.david@gmail.com>
2022-05-19 22:41:00 -0400

    Complete day1 part 1

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,18 +1,51 @@
 defmodule DAY1 do
   @moduledoc """
-  Documentation for `DAY1`.
+  The first order of business is to figure out how quickly the depth increases,
+  just so you know what you're dealing with - you never know if the keys will
+  get carried into deeper water by an ocean current or a fish or something.
   """
 
   @doc """
-  Hello world.
+  Given a sequence of measurements, count the number of times a measurement
+  increases from the previous measurement.
 
   ## Examples
 
-      iex> DAY1.hello()
-      :world
-
+      iex> DAY1.count_increasing([1, 2, 3])
+      2
+      iex> DAY1.count_increasing([3, 2, 1])
+      0
   """
-  def hello do
-    :world
+  def count_increasing(list) do
+    reduce(list, 0)
+  end
+
+  def reduce([head | tail], {prev, acc}) do
+    if head > prev do
+      reduce(tail, {head, acc + 1})
+    else
+      reduce(tail, {head, acc})
+    end
+  end
+
+  def reduce([head | tail], acc) do
+    reduce(tail, {head, acc})
+  end
+
+  def reduce([], {_, acc}) do
+    acc
+  end
+
+  def reduce([], acc) do
+    acc
+  end
+
+  def run_day1(input_file) do
+    {:ok, body} = File.read(input_file)
+    input = Enum.map(String.split(body), fn x -> String.to_integer(x) end)
+    result = count_increasing(input)
+    IO.puts(result)
+
+    result
   end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,7 +2,40 @@ defmodule DAY1Test do
   use ExUnit.Case
   doctest DAY1
 
-  test "greets the world" do
-    assert DAY1.hello() == :world
+  test "Empty Input" do
+    input = []
+    assert DAY1.count_increasing(input) == 0
+  end
+
+  test "Singleton Input" do
+    input = [1]
+    assert DAY1.count_increasing(input) == 0
+  end
+
+  test "Strictly decreasing" do
+    input = [3, 2, 1]
+    assert DAY1.count_increasing(input) == 0
+  end
+
+  test "Strictly increasing" do
+    input = [1, 2, 3]
+    assert DAY1.count_increasing(input) == 2
+  end
+
+  test "Day 1 Sample Input" do
+    input = [
+      199,
+      200,
+      208,
+      210,
+      200,
+      207,
+      240,
+      269,
+      260,
+      263
+    ]
+
+    assert DAY1.count_increasing(input) == 7
   end
 end
