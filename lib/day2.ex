bec5c66b2f86f0aa50f114c42319f8b3124f271e
David Goldstein <goldstein.g.david@gmail.com>
2022-05-21 11:47:56 -0400

    Add test cases for day2p2

--- lib/day2.ex
+++ lib/day2.ex
@@ -11,6 +11,19 @@ defmodule DAY2 do
   going. Our starting position and depth are both `0`.
 
   The answer to our puzzle is the product of the position and depth.
+
+  PART 2
+  ======
+  There is a third value that we need to track beyond horizontal position and
+  depth: `aim`, which also starts at `0`. Furthermore, the other commands are
+  more complex:
+  - `down X` increases aim by `X` units
+  - `up X` decreases aim by `X` units
+  - `forward X`
+    - increases horizontal position by `X` units
+    - increases depth by aim multiplied by `X` units
+
+  The answer to our puzzle is once again the product of the position and depth.
   """
 
   @input_file "input/day2.input"
@@ -21,6 +34,9 @@ defmodule DAY2 do
 
     part1_result = part1(cmds)
     IO.puts("The result for part 1 is: #{part1_result}")
+
+    part2_result = part2(cmds)
+    IO.puts("The result for part 2 is: #{part2_result}")
   end
 
   defp parse_body(body) do
@@ -41,14 +57,27 @@ defmodule DAY2 do
 
   @doc """
   Takes a list of {command, magnitude} pairs and returns the product of
-  the end position and depth.
+  the end position and depth, using the part 2 control scheme.
   """
-  def part1(commands) do
-    {pos, depth} = Enum.reduce(commands, {0, 0}, &reduce/2)
+  def part2(commands) do
+    {pos, depth} = Enum.reduce(commands, {0, 0}, &p2_reduce/2)
     pos * depth
   end
 
-  defp reduce({cmd, val}, {pos, depth}) do
+  defp p2_reduce({cmd, val}, {pos, depth}) do
+    {pos, depth}
+  end
+
+  @doc """
+  Takes a list of {command, magnitude} pairs and returns the product of
+  the end position and depth using the part 1 control scheme.
+  """
+  def part1(commands) do
+    {pos, depth} = Enum.reduce(commands, {0, 0}, &p1_reduce/2)
+    pos * depth
+  end
+
+  defp p1_reduce({cmd, val}, {pos, depth}) do
     case cmd do
       :forward -> {pos + val, depth}
       :down -> {pos, depth + val}
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -10,6 +10,57 @@ defmodule DAY2Test do
     {:forward, 2}
   ]
 
+  ########
+  # Part 2
+  #
+  test "P2: Empty Input" do
+    input = []
+    assert DAY2.part2(input) == 0
+  end
+
+  test "P2: Only Forward" do
+    input = [{:forward, 42}]
+    assert DAY2.part2(input) == 0
+  end
+
+  test "P2: Only Up and Down" do
+    input = [{:up, 42}, {:down, 41}]
+    assert DAY2.part2(input) == 0
+  end
+
+  test "P2: Forward then Aim" do
+    input = [{:forward, 1}, {:down, 5}, {:up, 2}]
+    assert DAY2.part2(input) == 0
+  end
+
+  test "P2: Aim then forward" do
+    input = [{:down, 2}, {:forward, 3}]
+    assert DAY2.part2(input) == 18
+  end
+
+  test "P2: Fish out of water" do
+    input = [{:up, 3}, {:forward, 2}]
+    assert DAY2.part2(input) == -12
+  end
+
+  test "P2: Even out" do
+    input = [{:down, 3}, {:forward, 1}, {:up, 6}, {:forward, 1}]
+    assert DAY2.part2(input) == 0
+  end
+
+  test "P2: Don't even out" do
+    input = [{:up, 3}, {:forward, 1}, {:down, 9}, {:forward, 1}]
+    assert DAY2.part2(input) == 6
+  end
+
+  test "P2: Sample Input" do
+    input = @sample_input
+    assert DAY2.part2(input) == 900
+  end
+
+  ########
+  # Part 1
+  #
   test "P1: Empty Input" do
     input = []
     assert DAY2.part1(input) == 0
